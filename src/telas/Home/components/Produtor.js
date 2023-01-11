b4d0d198d6f35c0c0f0f8529c3ebb8482c0109df
Mateus Gabriel da Silva <mateus.gabriel@vocedm.com.br>
2023-01-11 17:21:05 -0300

    Criado Hook, usando useReducer e finalizando app

--- src/telas/Home/components/Produtor.js
+++ src/telas/Home/components/Produtor.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import Estrelas from '../../../components/Estrelas'
 
 export default function Produtor({nome, imagem, distancia, estrelas}) {
-    const [selecionado, setSelecionado] = useState(false)
+    const [selecionado, inverterSelecionado] = useReducer(
+        (selecionado) => !selecionado,
+        false
+    )
     return <>
         <TouchableOpacity
             style={estilos.cartao}
-            onPress={() => setSelecionado(!selecionado)}
+            onPress={inverterSelecionado}
         >
             <Image style={estilos.imagem} source={imagem} accessibilityLabel={nome} />
             <View style={estilos.informacoes}>
